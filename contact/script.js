6ae3faf05568f8d6c4317cbdb516823442950e7e
Mu-jun <suo24691@gmail.com>
2025-01-04 23:01:05 +0900

    연속 전송 방지

--- contact/script.js
+++ contact/script.js
@@ -1,9 +1,13 @@
 const contactForm = document.getElementById('contact-us');
 const scriptURL =
   'https://script.google.com/macros/s/AKfycbz8wKsDoNPlMOKTXk-u6e4rNIFB-KGdt9JyI_t2Kt1ybZiXHhkR9wPYp9HU5Apqc4GY/exec';
+let submitting = false;
 
 async function onSubmit(e) {
   e.preventDefault();
+
+  if (submitting) return;
+  submitting = true;
 
   const formData = new FormData(e.target);
 
@@ -18,12 +22,15 @@ async function onSubmit(e) {
     console.log(res);
     if (response.ok) {
       alert('전송되었습니다');
+      window.location.reload();
     } else {
       alert('Error: ' + res.error);
     }
   } catch (error) {
     alert('Error: ' + error.message);
   }
+
+  submitting = false;
 }
 
 contactForm.addEventListener('submit', onSubmit);
